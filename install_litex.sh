0e8d9e5594ad63853e3f1529a513e5841b500ff9
gojimmypi <gojimmypi@gmail.com>
2020-03-07 22:00:42 -0800

    use install template code

--- install_litex.sh
+++ install_litex.sh
@@ -1,47 +1,19 @@
 #!/bin/bash
 #"***************************************************************************************************"
-# 
+#  common initialization
 #"***************************************************************************************************"
+# perform some version control checks on this file
 ./gitcheck.sh $0
-SAVED_CURRENT_PATH=$(pwd)
 
-#"***************************************************************************************************"
-# check that we are not running as root
-#"***************************************************************************************************"
-ls /root > /dev/null 2>&1
-EXIT_STAT=$?
-if [ $EXIT_STAT -ne 0 ];then
-  echo "Startup ulx3s toolchain. Security Context:" $(whoami) 2>&1 | tee -a  "$THIS_LOG"
-else
-  echo "Setup aborted. Do not run as root. Causes undesired install permissions. Enter sudo password as required during setup." 2>&1 | tee -a  "$THIS_LOG"
-  exit 2
-fi
+# initialize some environment variables and perform some sanity checks
+. ./init.sh
 
-if [ "$WORKSPACE" == "" ]; then
-  if grep -q Microsoft /proc/version; then
-    WORKSPACE=/mnt/c/workspace # put your WSL linux path here. Place outside of WSL file system for easy refresh
-  else
-    WORKSPACE=~/workspace  # put your pure linux workspace parent directory here. avoid spaces in path
-  fi
-fi
+# we don't want tee to capture exit codes
+set -o pipefail
 
-mkdir -p "$WORKSPACE"
-mkdir -p "$WORKSPACE/install_logs"
-
-LOG_SUFFIX=$(date +"%Y%m%d_%H%M%S")
-
-# strip any path from $0 to get just the file name of this script, stripping any CR, LF, Tabs
-THIS_FILE_NAME=$(basename $0 .sh | tr --delete '\t\r\n' )
-
-LOG_DIRECTORY="$WORKSPACE/install_logs"
-
-cd "$WORKSPACE"
-echo ""
-echo "Installing to $(pwd)"
 #"***************************************************************************************************"
 # 
 #"***************************************************************************************************"
-THIS_LOG=$LOG_DIRECTORY"/"$THIS_FILE_NAME"_litex_"$LOG_SUFFIX".log"
 
 # LiteX system requirements (also requires riscv32-unknown-elf-gcc)
 sudo apt install build-essential device-tree-compiler wget git python3-setuptools --assume-yes 2>&1 | tee -a "$THIS_LOG"
