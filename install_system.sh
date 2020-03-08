8ab2bea725c50aca8e103efd770155b70892b3e5
gojimmypi <gojimmypi@gmail.com>
2020-03-08 13:15:36 -0700

    more logging and error checking

--- install_system.sh
+++ install_system.sh
@@ -19,7 +19,10 @@ echo "**************************************************************************
 echo "update/upgrade current system. Saving log to $THIS_LOG"
 echo "***************************************************************************************************"
 sudo apt-get update --assume-yes        2>&1 | tee -a "$THIS_LOG"
+$SAVED_CURRENT_PATH/check_for_error.sh    $?          "$THIS_LOG"
+
 sudo apt-get upgrade --assume-yes       2>&1 | tee -a "$THIS_LOG"
+$SAVED_CURRENT_PATH/check_for_error.sh    $?          "$THIS_LOG"
 
 echo "***************************************************************************************************"
 echo "git install and config. Saving log to $THIS_LOG"
@@ -29,6 +32,7 @@ echo "**************************************************************************
 #
 # it is unlikely to cause harm for these repositories, unless for some reason your firewall is blocking HTTPS
 sudo apt-get install git --assume-yes   2>&1 | tee -a "$THIS_LOG"
+$SAVED_CURRENT_PATH/check_for_error.sh    $?          "$THIS_LOG"
 
 # git config --global url.https://git.qemu.org/git/.insteadOf git://git.qemu-project.org/
 # git config --global url.https://anongit.freedesktop.org/git/.insteadOf git://anongit.freedesktop.org/
@@ -44,7 +48,8 @@ sudo apt-get install git --assume-yes   2>&1 | tee -a "$THIS_LOG"
 echo "***************************************************************************************************"
 echo " install x11 dependencies (only needed for WSL?)"
 echo "***************************************************************************************************"
-sudo apt install dbus-x11
+sudo apt install dbus-x11 --assume-yes
+$SAVED_CURRENT_PATH/check_for_error.sh    $?           "$THIS_LOG"
 
 cd "$WORKSPACE"
 echo "***************************************************************************************************"
@@ -58,21 +63,25 @@ echo "**************************************************************************
 sudo apt-get install build-essential clang bison flex libreadline-dev \
 					 gawk tcl-dev libffi-dev git mercurial graphviz   \
 					 xdot pkg-config libftdi-dev --assume-yes 2>&1 | tee -a "$THIS_LOG"
-$SAVED_CURRENT_PATH/check_for_error.sh $? "$THIS_LOG"
+$SAVED_CURRENT_PATH/check_for_error.sh                          $?          "$THIS_LOG"
 
 
 echo "***************************************************************************************************"
 echo "install Python and dependencies. Saving log to $THIS_LOG"
 echo "***************************************************************************************************"
-sudo apt-get install python python3 2>&1 | tee -a "$THIS_LOG"
-sudo apt-get install python3-pip --assume-yes 2>&1 | tee -a "$THIS_LOG"
-pip3 install database
+sudo apt-get install python python3  --assume-yes  2>&1 | tee -a "$THIS_LOG"
+$SAVED_CURRENT_PATH/check_for_error.sh               $?          "$THIS_LOG"
+
+sudo apt-get install python3-pip --assume-yes      2>&1 | tee -a "$THIS_LOG"
+$SAVED_CURRENT_PATH/check_for_error.sh               $?          "$THIS_LOG"
+
+pip3 install database                              2>&1 | tee -a "$THIS_LOG"
 
 echo "***************************************************************************************************"
 echo "install apio. Saving log to $THIS_LOG"
 echo "***************************************************************************************************"
-pip3 install -U apio 2>&1 | tee -a "$THIS_LOG"
-$SAVED_CURRENT_PATH/check_for_error.sh $? "$THIS_LOG"
+pip3 install -U apio                 2>&1 | tee -a "$THIS_LOG"
+$SAVED_CURRENT_PATH/check_for_error.sh $?          "$THIS_LOG"
 
 echo "***************************************************************************************************"
 echo "install nextpnr dependencies. Saving log to $THIS_LOG"
@@ -83,29 +92,30 @@ echo "**************************************************************************
 #   After this operation, 623 MB of additional disk space will be used.
 #
 sudo apt-get install libboost-all-dev python3-dev qt5-default clang-format libeigen3-dev --assume-yes 2>&1 | tee -a "$THIS_LOG"
-$SAVED_CURRENT_PATH/check_for_error.sh $? "$THIS_LOG"
+$SAVED_CURRENT_PATH/check_for_error.sh    $?          "$THIS_LOG"
 
 sudo apt-get install cmake --assume-yes 2>&1 | tee -a "$THIS_LOG"
-$SAVED_CURRENT_PATH/check_for_error.sh $? "$THIS_LOG"
+$SAVED_CURRENT_PATH/check_for_error.sh    $?          "$THIS_LOG"
 
 echo "***************************************************************************************************"
 echo "install RISCV system dependencies. Saving log to $THIS_LOG"
 echo "***************************************************************************************************"
-sudo apt-get install make        --assume-yes
-sudo apt-get install make-guile  --assume-yes
-sudo apt-get install libgmp3-dev --assume-yes
-sudo apt-get install libmpfr-dev --assume-yes
-sudo apt-get install libmpc-dev  --assume-yes
+sudo apt-get install make        --assume-yes      2>&1 | tee -a "$THIS_LOG"
+sudo apt-get install make-guile  --assume-yes      2>&1 | tee -a "$THIS_LOG"
+sudo apt-get install libgmp3-dev --assume-yes      2>&1 | tee -a "$THIS_LOG"
+sudo apt-get install libmpfr-dev --assume-yes      2>&1 | tee -a "$THIS_LOG"
+sudo apt-get install libmpc-dev  --assume-yes      2>&1 | tee -a "$THIS_LOG"
 
 sudo apt-get install autoconf automake autotools-dev curl libmpc-dev \
         libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo \
-    gperf libtool patchutils bc zlib1g-dev git libexpat1-dev --assume-yes
+    gperf libtool patchutils bc zlib1g-dev git libexpat1-dev --assume-yes  2>&1 | tee -a "$THIS_LOG"
 $SAVED_CURRENT_PATH/check_for_error.sh $? "$THIS_LOG"
 
 echo "***************************************************************************************************"
 echo "install iverilog. Saving log to $THIS_LOG"
 echo "***************************************************************************************************"
-sudo apt-get install iverilog
+sudo apt-get install iverilog --assume-yes  2>&1 | tee -a "$THIS_LOG"
+$SAVED_CURRENT_PATH/check_for_error.sh        $?          "$THIS_LOG"
 
 
 
@@ -117,24 +127,24 @@ echo "**************************************************************************
 # 0 upgraded, 1 newly installed, 0 to remove and 1 not upgraded.
 # Need to get 2878 kB of archives.
 # After this operation, 13.1 MB of additional disk space will be used.
-sudo apt-get install verilator   2>&1 | tee -a "$THIS_LOG"
-$SAVED_CURRENT_PATH/check_for_error.sh $? "$THIS_LOG"
+sudo apt-get install verilator       2>&1 | tee -a "$THIS_LOG"
+$SAVED_CURRENT_PATH/check_for_error.sh $?          "$THIS_LOG"
 
 if grep -q Microsoft /proc/version; then
   echo "***************************************************************************************************"
   echo "WSL detected; Install x86_64-w64-mingw32-gcc; Saving log to $THIS_LOG"
   echo "***************************************************************************************************"
-  sudo apt-get install mingw-w64 2>&1 | tee -a "$THIS_LOG"
-  $SAVED_CURRENT_PATH/check_for_error.sh $? "$THIS_LOG"
+  sudo apt-get install mingw-w64   --assume-yes    2>&1 | tee -a "$THIS_LOG"
+  $SAVED_CURRENT_PATH/check_for_error.sh             $?          "$THIS_LOG"
 fi
 
 echo "***************************************************************************************************"
 echo "update/upgrade current system again. Saving log to $THIS_LOG"
 echo "***************************************************************************************************"
 sudo apt-get update --assume-yes        2>&1 | tee -a "$THIS_LOG"
-$SAVED_CURRENT_PATH/check_for_error.sh $? "$THIS_LOG"
+$SAVED_CURRENT_PATH/check_for_error.sh    $?          "$THIS_LOG"
 
 sudo apt-get upgrade --assume-yes       2>&1 | tee -a "$THIS_LOG"
-$SAVED_CURRENT_PATH/check_for_error.sh $? "$THIS_LOG"
+$SAVED_CURRENT_PATH/check_for_error.sh    $?          "$THIS_LOG"
 
 echo "Completed $0 " | tee -a "$THIS_LOG"
