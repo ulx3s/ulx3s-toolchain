e18bdac568b52efafef5246483ef3fd1d405ee54
gojimmypi <gojimmypi@gmail.com>
2020-03-09 18:52:46 -0700

    comments added

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
+echo "installing git..."
 sudo apt-get install git --assume-yes
+
 mkdir -p ~/workspace
 cd ~/workspace
+
 git clone https://github.com/gojimmypi/ulx3s-toolchain.git
 cd ulx3s-toolchain
+
+echo "running ./install_all.sh"
 chmod +x ./install_all.sh
 ./install_all.sh
+
+echo "Done!"
