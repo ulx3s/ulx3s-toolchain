b49a58b07b218893c038fbf07c39b2940c34244c
gojimmypi <gojimmypi@gmail.com>
2020-03-09 18:53:50 -0700

    added some new installs. changed sequence

--- install_all.sh
+++ install_all.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 #"***************************************************************************************************"
+#  ensure all the scripts here are executable
+#"***************************************************************************************************"
+chmod +x install_set_permissions.sh
+./install_set_permissions.sh
+
+#"***************************************************************************************************"
 #  common initialization
 #"***************************************************************************************************"
 # perform some version control checks on this file
@@ -24,47 +30,72 @@ fi
 
 echo "Install all ULX3S toolchains. Edit parameters in init.sh"
 echo ""
-echo "logs saved to $LOG_DIRECTORY" 
+echo "logs saved to $LOG_DIRECTORY"
 echo ""
 echo ""
 read -p "Press enter to continue, or Ctrl-C to abort."
 
 cd $SAVED_CURRENT_PATH
-chmod +x install_set_permissions.sh
-./install_set_permissions.sh
 
+# system updates and dependencies
 ./install_system.sh
 
+# pre-compiled binaries
 ./install_ulx3s-bin.sh
 
+# ESP32
 ./install_esp32.sh
 ./install_openocd-esp32.sh
 
+# RISC-V
 ./install_riscv-gnu-toolchain-rv32i.sh
 ./install_picorv32_riscv32i.sh
 ./install_openocd.sh
 
+# yosys / prjtrellis / nextpnr
 ./install_yosys.sh
 ./install_prjtrellis.sh
 ./install_nextpnr.sh
 
+# verilator and icestorm
 ./install_verilator.sh
 ./install_icestorm.sh
+
+# litex
 ./install_litex.sh
 
 # not maintained anymore, but arachne-pnr needs icestorm
 ./install_arachne-pnr.sh
 
-
+# more examples and tools
+./install_rxrbln-picorv32.sh
 ./install_ujprog.sh
 ./install_blinky.sh
 ./install_fpga_odysseus.sh
+
+./install_ulx3s.sh
+./install_ulx3s-misc.sh
 ./install_ulx3s-examples.sh
-./install_rxrbln-picorv32.sh
+
+# run a synthesis
+./install_litex-ulx3s.sh
 
 echo "***************************************************************************************************"
 echo "update current system again. Saving log to $THIS_LOG"
 echo "***************************************************************************************************"
-sudo apt-get update --assume-yes        2>&1 | tee -a "$THIS_LOG"
+sudo apt-get upgrade --assume-yes        2>&1 | tee -a "$THIS_LOG"
+
+if [ "$(sudo cat /etc/sudoers | grep timestamp_timeout)" != "" ]; then
+  echo ""
+  echo "WARNING: timestamp_timeout found in /etc/sudoers"
+  echo ""
+  echo "If you did this in order to install the toolchain unattended, consider removing it now with: sudo visudo"
+  echo ""
+fi
+
+echo ""
+echo "See logs in $LOG_DIRECTORY"
+echo ""
+
 
 echo "Completed $0 " | tee -a "$THIS_LOG"
