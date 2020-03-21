9e9e24c77b2864c337a26f958ac248b9ef97670a
gojimmypi <gojimmypi@gmail.com>
2020-03-21 09:00:41 -0700

    fixed update / refresh

--- install_riscv-gnu-toolchain-rv32i.sh
+++ install_riscv-gnu-toolchain-rv32i.sh
@@ -21,15 +21,26 @@ sudo apt-get install autoconf automake autotools-dev curl libmpc-dev \
         libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo \
     gperf libtool patchutils bc zlib1g-dev git libexpat1-dev  --assume-yes          2>&1 | tee -a "$THIS_LOG"
 
-sudo mkdir /opt/riscv32i
+sudo mkdir -p /opt/riscv32i
 sudo chown $USER /opt/riscv32i
 
 # setup the path, even  though there may be nothing in it yet
-. $SAVED_CURRENT_PATH/set_riscv_path.sh
+cd ulx3s-toolchain
+. ./set_riscv_path.sh
+
+# note we called init.sh when setting the path, so we are back in the $WORKSPACE directory now
 
 echo "***************************************************************************************************"
 echo " riscv-gnu-toolchain-rv32i. Saving log to $THIS_LOG"
 echo "***************************************************************************************************"
+
+# users sitting behind a firewall may need these:
+
+git config --global url.https://github.com/.insteadOf                  git://github.com/
+git config --global url.https://git.qemu.org/git/.insteadOf            git://git.qemu-project.org/
+git config --global url.https://anongit.freedesktop.org/git/.insteadOf git://anongit.freedesktop.org/
+git config --global url.https://github.com/riscv.insteadOf             git://github.com/riscv
+
 if [ ! -d "$WORKSPACE"/riscv-gnu-toolchain-rv32i ]; then
   git clone https://github.com/riscv/riscv-gnu-toolchain riscv-gnu-toolchain-rv32i  2>&1 | tee -a "$THIS_LOG"
   $SAVED_CURRENT_PATH/check_for_error.sh $? "$THIS_LOG"
@@ -37,25 +48,19 @@ if [ ! -d "$WORKSPACE"/riscv-gnu-toolchain-rv32i ]; then
   cd riscv-gnu-toolchain-rv32i
   git checkout 411d134
 
-  # if you see fatal: clone of 'git://  ... 
-  # users sitting behind a firewall may need these:
-  git config --global url.https://github.com/.insteadOf                  git://github.com/
-  git config --global url.https://git.qemu.org/git/.insteadOf            git://git.qemu-project.org/
-  git config --global url.https://anongit.freedesktop.org/git/.insteadOf git://anongit.freedesktop.org/
-  git config --global url.https://github.com/riscv.insteadOf             git://github.com/riscv
-
   echo "This next step takes a long time. Be patient:"
   git submodule update --init --recursive
   $SAVED_CURRENT_PATH/check_for_error.sh $? "$THIS_LOG"
- else
+else
    cd riscv-gnu-toolchain-rv32i
    git fetch                                                                         2>&1 | tee -a "$THIS_LOG"
-   git pull                                                                          2>&1 | tee -a "$THIS_LOG"
-   $SAVED_CURRENT_PATH/check_for_error.sh $? "$THIS_LOG"
-   git checkout 411d134
+
+   # we are in a detached head at 411d134 so cannot pull
+   # git pull                                                                        2>&1 | tee -a "$THIS_LOG"
+   # $SAVED_CURRENT_PATH/check_for_error.sh $? "$THIS_LOG"
+   # git checkout 411d134
 
    git submodule update --recursive                                                  2>&1 | tee -a "$THIS_LOG"
-   git checkout 411d134                                                              2>&1 | tee -a "$THIS_LOG"
 fi
 
 # after we have new (or fresh) files, build
@@ -71,4 +76,5 @@ make -j$(nproc)                                                                 
 $SAVED_CURRENT_PATH/check_for_error.sh $? "$THIS_LOG"
 
 echo "Completed $0 "                                                  | tee -a "$THIS_LOG"
- 
+
+
