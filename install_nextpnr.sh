d67bb186912f9ce59e5ac7fe9621badccbab2b5a
gojimmypi <gojimmypi@gmail.com>
2020-05-23 11:52:11 -0700

    show more details for libQt5Core.so.5 strip

--- install_nextpnr.sh
+++ install_nextpnr.sh
@@ -112,11 +112,16 @@ if [ $EXIT_STAT -ne 0 ];then
     while true; do
       echo "Do you wish to try to fix libQt5Core.so.5 with:"
       echo ""
-      echo "  strip --remove-section=.note.ABI-tag /usr/./lib/x86_64-linux-gnu/libQt5Core.so.5"
+      echo "  strip --remove-section=.note.ABI-tag ?"
       echo ""
       read -p "Try this?" yn
       case $yn in
-          [Yy]* ) echo "Fixing!"; sudo strip --remove-section=.note.ABI-tag /usr/./lib/x86_64-linux-gnu/libQt5Core.so.5; break;;
+          [Yy]* ) echo "Fixing!"
+                  echo "Searching for libQt5Core.so.5 in /usr/. ..."
+                  THIS_LIB=$(find /usr/. -name "libQt5Core.so.5")
+                  echo "Found: $THIS_LIB"
+                  sudo strip --remove-section=.note.ABI-tag "$THIS_LIB"
+                  break;;
           [Nn]* ) echo "Fix NOT applied."; break;;
           * ) echo "Please answer yes or no.";;
       esac
